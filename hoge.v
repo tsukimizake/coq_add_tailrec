37306352e4be0692cb94d7fa318f95366148a642
shomasd <shomasd@ueda.info.waseda.ac.jp>
2020-02-13 12:48:24 +0900

    add_tailrec

--- hoge.v
+++ hoge.v
@@ -28,19 +28,57 @@ Proof.
   reflexivity.
 Qed.
 
+Theorem add_comm : forall l r : Nat, add l r = add r l.
+Proof.
+  intros.
+  induction l.
+  simpl.
+  induction r.
+  reflexivity.
+  simpl.
+  rewrite <- IHr.
+  reflexivity.
+  simpl.
+  rewrite IHl.
+  assert (forall x y, s(add x y) = add x (s y)).
+  intros.
+  induction x.
+  reflexivity.
+  simpl.
+  rewrite <- IHx.
+  reflexivity.
+  apply H.
+Qed.
+  
 Fixpoint add_tr(l:Nat)(r:Nat) : Nat :=
   match l with
   | z => r
-  | s l' => add l' (s r)
+  | s l' => add_tr l' (s r)
 end.
 
-Lemma add_tailrec_succ : forall l r : Nat, s (add_tr l r) = add_tr l (s r).
+Eval simpl in add_tr (s z) (s (s z)).
+
+Lemma add_tailrec_succ : forall l r : Nat, add_tr (s l) r = add_tr l (s r).
 Proof.
   intros.
   induction l.
   simpl.
   reflexivity.
   simpl.
-  rewrite <- IHl.
   reflexivity.
 Qed.
+
+Theorem add_tailrec_comm : forall l r : Nat, add_tr l r = add_tr r l.
+Proof.
+  intros.
+  induction l.
+  simpl.
+  assert (forall x, add_tr z x = add_tr x z).
+  induction x.
+  reflexivity.
+  simpl.
+  assert (forall x, add_tr x (s z) = s (add_tr x z)).
+  induction x0.
+  simpl.
+  reflexivity.
+  
